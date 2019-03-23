ee75171d9470bf67f89f316b8f7de06c1d5ade7d
Home <dorin.home@outlook.com>
2019-03-23 15:07:17 +0200

    TIMERS\NON_BLOCKING\NeoTimer
    - Create one NeoTimer
    - Execute some code at each 2222 milliseconds
    - In rest of time count how many times loop() function runs and print
    this number when timer code is triggered

--- TIMERS/NON_BLOCKING/NeoTimer/NeoTimer.cpp
+++ TIMERS/NON_BLOCKING/NeoTimer/NeoTimer.cpp
@@ -1,9 +1,31 @@
 #include <Arduino.h>
+#include <neotimer.h>
+
+/***********************************************************************
+ *                        MACRO DEFINES
+ ***********************************************************************/
+#define	ONBOARD_LED				13
+
+/***********************************************************************
+ *                        GLOBAL DATA
+ ***********************************************************************/
+Neotimer		timer1		= Neotimer(2222);
+unsigned long   LoopCalls 	= 0;
 
 void setup() {
+	Serial.begin(57600);
+	pinMode(ONBOARD_LED, OUTPUT);
 
 }
 
 void loop() {
 
+	if(  timer1.repeat(20) ) {
+		Serial.println( String("Timer trigger: ") + millis() );
+		Serial.println( String("Loop called for ") + LoopCalls + " times");
+		LoopCalls = 0;
+	}
+	else {
+		LoopCalls ++;
+	}
 }
