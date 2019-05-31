360f172ccedc55af713bada86492ff7956ab9526
Home <dorin.home@outlook.com>
2019-05-31 20:36:53 +0300

    THREADS/SolarTrackSystem/
    - Add "steps measurements" capabilities to Nema17 class

--- THREADS/SolarTrackSystem/Nema17Motor.cpp
+++ THREADS/SolarTrackSystem/Nema17Motor.cpp
@@ -14,6 +14,10 @@ Nema17Motor::Nema17Motor(uint8_t pinDir, uint8_t pinStep, uint8_t pinEnable)
 	this->pin_enable = pinEnable;
 	this->step_speed = DEFAULT_SPEED;
 	this->is_Enabled = false;
+	this->stepsDone = 0;
+	this->is_Measure_Enabled = false;
+	this->position = 0;
+	this->direction = DIRECTION_BACKWARD;
 
 	// configure motor pins as output
 	pinMode(pinDir   , OUTPUT);
@@ -25,3 +29,34 @@ Nema17Motor::~Nema17Motor() {
 	// TODO Auto-generated destructor stub
 }
 
+void Nema17Motor::goToPosition(int16_t targetPos) {
+	if(targetPos < this->position) {
+		this->goBackward();
+	}
+	else {
+		this->goForward();
+	}
+
+	while(targetPos != this->position) {
+		this->step();
+	}
+}
+
+void Nema17Motor::setSpeed(uint16_t speed)
+{
+	// speed must be in range [80,700] [steps/second]
+	// convert this speed to delay between steps
+	uint16_t tmpSpeed = 0;
+
+	tmpSpeed = (1000000UL/(2*speed));
+
+	// step delay must be a value in range [700, 6300]
+	// make this check
+	if( 700 <= tmpSpeed && tmpSpeed <= 6300 ) {
+		// speed is valid; apply this speed
+		this->step_speed = tmpSpeed;
+	}
+	else {
+		// speed is not valid; ignore it
+	}
+}
--- THREADS/SolarTrackSystem/Nema17Motor.h
+++ THREADS/SolarTrackSystem/Nema17Motor.h
@@ -10,32 +10,56 @@
 
 #include <Arduino.h>
 
-#define DEFAULT_SPEED	((uint16_t) 2500)
+#define DEFAULT_SPEED			((uint16_t) 2500)
+#define DIRECTION_FORWARD		1
+#define DIRECTION_BACKWARD		-1
 
 class Nema17Motor {
 private:
 	uint8_t		pin_dir;
 	uint8_t		pin_step;
 	uint8_t		pin_enable;
+	int8_t		direction;
+	int16_t		position;
 	uint16_t 	step_speed; // [us]
 	boolean		is_Enabled;
+	boolean		is_Measure_Enabled;
+	int16_t		stepsDone;
+
 public:
 	Nema17Motor(uint8_t pinDir, uint8_t pinStep, uint8_t pinEnable);
 	virtual ~Nema17Motor();
 
-	inline void setSpeed(uint16_t speed) { this->step_speed = speed; }
+	void goToPosition(int16_t targetPos);
+	inline int16_t getPosition() const { return this->position; }
+	void setSpeed(uint16_t speed);
 	inline void turnOn() { digitalWrite(this->pin_enable, LOW); this->is_Enabled = true; }
 	inline void turnOff() { digitalWrite(this->pin_enable, HIGH); this->is_Enabled = false; }
 	inline bool isOn() const { return (this->is_Enabled == true); }
-	inline void goForward() const { digitalWrite(this->pin_dir, HIGH); }
-	inline void goBackward() const { digitalWrite(this->pin_dir, LOW); }
-	inline void stepHigh() const { digitalWrite(this->pin_step, HIGH); }
-	inline void stepLow() const { digitalWrite(this->pin_step, LOW); }
-	inline void step() const {
+	inline void goForward() { digitalWrite(this->pin_dir, HIGH); this->direction = DIRECTION_FORWARD; }
+	inline void goBackward() { digitalWrite(this->pin_dir, LOW); this->direction = DIRECTION_BACKWARD; }
+	inline void stepHigh() {
+		digitalWrite(this->pin_step, HIGH);
+	}
+	inline void stepLow() {
+		digitalWrite(this->pin_step, LOW);
+		this->stepsDone += (int16_t)(this->is_Measure_Enabled);
+		this->position += this->direction;
+	}
+	inline void step() {
 		this->stepHigh(); delayMicroseconds(this->step_speed);
 		this->stepLow();  delayMicroseconds(this->step_speed);
 	}
+	inline void move(uint16_t howMany) {
+		while((howMany--) > 0) {
+			this->step();
+		}
+	}
 	inline uint16_t getSpeed() const { return this->step_speed; }
+	inline void stepMeasureStart() { this->is_Measure_Enabled = true; this->stepsDone = 0; }
+	inline void stepMeasureStop() { this->is_Measure_Enabled = false; }
+	inline int16_t getStepMeasurement() const { return this->stepsDone; }
+	inline void setCenterPosition() { this->position = 0; }
 };
 
 #endif /* NEMA17MOTOR_H_ */
